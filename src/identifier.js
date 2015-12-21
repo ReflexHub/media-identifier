64e8b5e96707b14c613b2937ca1b9c3d99da40d7
Amish Shah <amishshah.2k@gmail.com>
2015-12-21 23:13:28 +0000

    Set up basic structure

---
+++ src/identifier.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const natural = require("natural");
+const cheerio = require("cheerio");
+const request = require("superagent");
+
+class Identifier {
+	constructor(options) {
+
+		options.hierachy = options.hierachy || [
+			"FILENAME"
+		];
+
+		this.options = options;
+	}
+
+	identify(path_to_file) {
+
+		let file_name = path.basename(path_to_file);
+
+	}
+}
+
+module.exports = Identifier;
